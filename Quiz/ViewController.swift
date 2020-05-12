eb197fe4a74210d7455d07f5fc019f3177a6ea65
yukiees <yukiii8989@gmail.com>
2020-05-12 20:53:17 +0900

    quiz app remake

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func toQuiz() {
+        performSegue(withIdentifier: "quiz", sender: nil)
+    }
+    
 }
 
